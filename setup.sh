621e571ab2194f0218ffcdca1103b23a202236d5
shikhar <shikharvashishth@gmail.com>
2019-01-24 13:19:28 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-# Install python dependencies
-pip install -r requirements.txt
+echo "Create log directory"
+mkdir log
 
-# Extracts Citeseer, Cora, Pubmed, and Cora-ML datasets
+echo "Extracts Citeseer, Cora, Pubmed, and Cora-ML datasets"
 unzip data.zip
 
-# Extracts best saved models on all four datasets
+echo "Extracts best saved models on all four datasets"
 unzip checkpoints.zip
 
-# Create log directory
-mkdir log
+echo "Install python dependencies"
+pip install -r requirements.txt
